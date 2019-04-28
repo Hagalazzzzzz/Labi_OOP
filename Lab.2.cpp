cef8f22687d08155997695ec9063c05b0916cd43
Hagalazzzzzz <kuzaeva.oa@gmail.com>
2019-04-28 22:14:43 +0300

    Вторая лаба

---
+++ Lab.2.cpp
@@ -0,0 +1,51 @@
+2.1
+
+//#include <iostream>
+//#include <iomanip>
+////for setw()
+//using namespace std;
+//int main()
+//{
+//    unsigned long n;
+////number
+//    cout <<"\nEnter a number:";
+//    cin >>n; //get
+//
+//    for(int j=1; j<=200; j++) //loop from 1 to 200
+//    {
+//        cout << setw(5) << j*n << "\t"; //print multiple of n
+//        if( j%10 == 0 )
+////every 10 numbers,
+//            cout << endl;
+////start new line
+//    }
+//    return 0;
+//}
+
+// 2.2
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int response;
+//    double temper;
+//    cout << "\nType 1 to convert fahrenheit to celsius,"
+//    << "\n 2 to convert celsius to fahrenheit: ";
+//    cin >> response;
+//    if( response == 1 )
+//    {
+//        cout << "Enter temperature in fahrenheit: ";
+//
+//        cin >> temper;
+//        cout << "In celsius that "<< 5.0/9.0*(temper-32.0);
+//    }
+//    else
+//    {
+//        cout << "Enter temperature in celsius: ";
+//        cin >> temper;
+//        cout <<"In fahrenheit that "<< 9.0/5.0*temper + 32.0;
+//    }
+//    cout << endl;
+//    return 0;
+//}
+
